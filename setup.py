c54b514463330795c76538e631f865e577e00f10
Alexander Esacalona Fernández <alexescalonafernandez@gmail.com>
2018-02-23 11:36:16 -0500

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='service-locator',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/alexescalonafernandez/service-locator',
+    license='MIT',
+    author='Alexander Escalona Fernández',
+    author_email='alexescalonafernandez@gmail.com',
+    description='Python implementation of service locator pattern based on java CDI and ServiceLoader api.'
+)
