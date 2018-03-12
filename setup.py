9b3887a08c55d27943e3fa9961e0377ce10295ab
Alexander Esacalona Fernández <alexescalonafernandez@gmail.com>
2018-03-12 15:44:13 -0400

    update setup.py file

--- setup.py
+++ setup.py
@@ -13,13 +13,12 @@ setup(
     description='Python implementation of service locator pattern based on java CDI and ServiceLoader api.',
     long_description_markdown_filename='README.md',
     classifiers=[
-            'Environment :: Environment Independent',
             'Intended Audience :: Developers',
-            'License :: MIT License',
+            'License :: OSI Approved :: MIT License',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
-            'Topic :: Pattern Design :: Service Locator',
+            'Topic :: Utilities',
         ],
 )
